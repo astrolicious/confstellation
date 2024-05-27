9413e40dee59d48b937d2c29a3c52592f03d8b3b
Alexander Niebuhr <alexander@nbhr.io>
2024-05-27 18:46:07 -0400

    wip

---
+++ apps/demo/astro.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import MyTheme from '@astrolicious/confstellation';
+
+export default defineConfig({
+	integrations: [
+		MyTheme({
+			config: {
+				title: 'My Awesome Theme',
+				description: 'My awesome theme is currently under construction!',
+			},
+			pages: {},
+			overrides: {},
+		}),
+	],
+});
---
+++ apps/demo/src/env.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="../.astro/@astrolicious/confstellation.d.ts" />
+/// <reference types="../.astro/.d.ts" />
+/// <reference types="../.astro/my-theme.d.ts" />
